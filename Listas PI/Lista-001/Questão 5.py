494896a35272905a72b3076650157666be2ea070
Mikael Nitai <mikaelnitaicderose@gmail.com>
2026-01-18 17:58:22 -0300

    Lista-01: questão 5

---
+++ Listas PI/Lista-001/Questão 5.py
@@ -0,0 +1,6 @@
+# 5 - Faça um programa que receba uma frase e converta tudo em maiúsculo e em minúsculo.
+
+palavra=input("Digite uma palavra: ")
+# print(palavra.upper())
+# print(palavra.lower())
+print(f"A palavra informada foi {palavra}, em uppercase: {palavra.upper()} e em Lowercase {palavra.lower()}")
