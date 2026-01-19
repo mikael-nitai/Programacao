1ad7398c34abc3e420ba2c3ebc50ad695b0f098e
Mikael Nitai <mikaelnitaicderose@gmail.com>
2026-01-19 20:58:41 -0300

    Lista-03a: questão 6

---
+++ Listas PI/Lista-03a/Q6.py
@@ -0,0 +1,9 @@
+#6 - Escreva um programa em Python que mostre a tabuada de um número qualquer, dado como entrada.
+numero= int(input("Escolha um numero: "))
+
+i = 1
+
+for tabuada in range(10):
+    tabuada= numero * i
+    print(f"{numero} * {i} = {tabuada}")
+    i = i + 1
