75728b3b491c0cf73bd1d58a5c1b54575ffd14c0
Mikael Nitai <mikaelnitaicderose@gmail.com>
2026-01-20 11:46:16 -0300

    funções questão 3

---
+++ Listas PI/Lista-função/Q3.py
@@ -0,0 +1,6 @@
+#3 - Faça uma função em Python que mostre o último caractere de uma String
+def ultimo_caractere(texto):
+    return texto[-1]
+
+mensagem=input("Digite uma mensagem para exxibir o ultimo caractere")
+print(ultimo_caractere(mensagem))   
