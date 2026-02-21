4da88ec6e26df54124808d353c7a9856db25e866
mikael-nitai <mikaelnitaicderose@gmail.com>
2026-02-21 16:10:07 -0300

    Q8 lista listas

---
+++ Listas PI/Lista-list/Q8.py
@@ -0,0 +1,9 @@
+#7 - Faça um código em Python que iterar sobre uma lista com pelo menos 5 frutas e mostre o seu resultado
+frutas= ["mamão", "melancia", "abacate", "uva", "morango"]
+
+for fruta in frutas:
+    print(fruta)
+print("-" * 15)
+#8 - Com base na resposta da questão anterior, faça um código em Python que ordene todos os elementos da lista de frutas.
+frutas_ordenadas = sorted(frutas)
+print(frutas_ordenadas)
