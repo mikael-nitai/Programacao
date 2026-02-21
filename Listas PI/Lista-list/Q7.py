0494aadc2ca921ef3d1900d918ceba79e4e9427b
mikael-nitai <mikaelnitaicderose@gmail.com>
2026-02-21 16:02:57 -0300

    Q7 lista listas

---
+++ Listas PI/Lista-list/Q7.py
@@ -0,0 +1,5 @@
+#7 - Faça um código em Python que iterar sobre uma lista com pelo menos 5 frutas e mostre o seu resultado
+frutas= ["mamão", "melancia", "abacate", "uva", "morango"]
+
+for fruta in frutas:
+    print(fruta)
