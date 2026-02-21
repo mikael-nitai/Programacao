be3c70322a88962d59df78aa00ada344a89ced65
mikael-nitai <mikaelnitaicderose@gmail.com>
2026-02-21 18:18:59 -0300

    Q9 lista tuplas

---
+++ Listas PI/Lista-tuplas/Q9.py
@@ -0,0 +1,11 @@
+# 9 - Faça um programa em Python que receba uma tupla: ("Ane", Bia", "Cida"), 
+# depois converta-a em uma lista e adicione o elemento "Diana". 
+# Por fim, converta novamente para uma tupla.
+
+nomes = ("Ane", "Bia", "Cida")
+lista= list(nomes)
+lista.append("Diana")
+nomes= tuple(lista)
+
+print(nomes)
+print(type(nomes))
