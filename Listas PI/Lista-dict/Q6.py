9990900db905504233c5464e5dd46b0b152a0164
mikael-nitai <mikaelnitaicderose@gmail.com>
2026-02-21 19:42:07 -0300

    atualização

---
+++ Listas PI/Lista-dict/Q6.py
@@ -0,0 +1,8 @@
+#6 - Faça um programa em Python que conte quantas chaves um dicionário possui.
+dicionario = {"país" : "brasil" ,
+              "estado" : "são paulo" ,
+              "cidade" : "Atibaia"
+}
+
+qtd_chaves= len(dicionario)
+print(qtd_chaves)
