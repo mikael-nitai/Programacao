82579ef5bc6bf9d79dd86f12e0b0e86c12f54ec5
mikael-nitai <mikaelnitaicderose@gmail.com>
2026-02-21 19:21:26 -0300

    Q4 lista dict

---
+++ Listas PI/Lista-dict/Q4.py
@@ -0,0 +1,8 @@
+#4 - Faça um programa em Python que verifique se uma chave pertence a um dicionário.
+
+dicionario = {"país" : "brasil" ,
+              "estado" : "são paulo" ,
+              "cidade" : "Atibaia"
+}
+if "cidade" in dicionario:
+    print("A chave existe")
