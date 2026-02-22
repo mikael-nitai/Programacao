4027c4d132b1372e0d140be40ad0d14ef96b079e
mikael-nitai <mikaelnitaicderose@gmail.com>
2026-02-21 23:17:00 -0300

    Q9 dict

--- Listas PI/Lista-dict/Q8.py
+++ Listas PI/Lista-dict/Q8.py
@@ -2,10 +2,10 @@
 # como chaves as vogais e seus valores a posição do alfabeto.
 alfabeto= "abcdefghijklmnopqrstuvwxyz"
 vogais= {}
-consoantes= []
 
 for v in alfabeto:
     posicao = alfabeto.index(v)
     if v in "aeiou":
         vogais[v] = alfabeto.index(v) + 1
+
 print(vogais)
---
+++ Listas PI/Lista-dict/Q9.py
@@ -0,0 +1,9 @@
+#9 - Faça um programa em Python que some todos os valores contidos dentro do 
+# dicionário a seguir: dicionario = {“a” : 1, “b” : 2, “c” : 3}
+dicionario = {"a" : 1 , "b" : 2 , "c" : 3}
+soma= 0
+
+for i in dicionario.values():
+    soma += i
+
+print(f"A soma dos valores do dicionário é {soma}")
